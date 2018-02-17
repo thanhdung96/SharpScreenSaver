a480b9b0ccd7c36cd7a5847b8fcfa0632e3c5c11
thanhdung96 <14dh110224@st.huflit.edu.vn>
2018-02-17 18:22:58 +0700

    init new branch

---
+++ SScreenSaver/Definitions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace SScreenSaver
+{
+	public partial class MainForm
+	{
+		private const int TOTAL_PANELS = 16;
+		private const int DIMENSION = 4;
+		private const int MAX_DELAY = 5;
+		private const int MIN_DELAY = 2;
+		private const int MAX_COLOR = 255;
+		private const int MIN_COLOR = 0;
+		private int CurrentIndex = 0;
+		Random rd = new Random();
+
+		private List<byte> PanelDelay = new List<byte>();
+
+		public TimeSpan TimeoutToHide { get; private set; }
+		public DateTime LastMouseMove { get; private set; }
+		public bool IsHidden { get; private set; }
+	}
+}
